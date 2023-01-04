0a691deb5eb129da453780087694aa5e8cbfad0b
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2023-01-03 20:08:08 -0500

    per

---
+++ .history/src/ftn/listar_20230103131516.js
@@ -0,0 +1,43 @@
+const contenido = document.getElementById("contenido");
+const nombreTarea = document.getElementById("nDBtarea");
+const miItemActual = document.getElementById("miItemActual");
+
+console.log(miItemActual.toggleAttribute)
+async function misDatos(data) {
+  contenido.innerHTML = "";
+  await data.map((item, index) => {
+    // console.log(item._id)
+    contenido.innerHTML += `
+            <tr>
+              <th id="miItemActual"  scope="row">${index + 1}</th>
+              
+              <td id="nDBtarea">${item.name}</td>
+              <td>${item.descripcion}</td>
+              <td>
+                <select class="form-select" aria-label="Default select example">
+                  <option selected>${
+                    item.statusTarea ? "pendiente" : "hecho"
+                  }</option>
+                  <option value="1">Hecho</option>
+                </select>
+              </td> 
+              <td onclick ="deleteTask('${
+                item._id
+              }')"><button  id="eliminarBtn" class="btn btn-danger">
+            <i class="fa-solid fa-trash-can"></i>
+          </button></td>
+        `;
+  });
+}
+
+async function deleteTask(id) {
+  console.log(id);
+  const response = await fetch(`http://localhost:3000/api/tareas/${id}`, {
+    method: "DELETE",
+  }).then(getDatos);
+  if (response.ok) {
+    console.log("efectivo");
+  } else {
+    console.log("erro");
+  }
+}
---
+++ .history/src/ftn/listar_20230103171014.js
@@ -0,0 +1,51 @@
+const contenido = document.getElementById("contenido");
+const nombreTarea = document.getElementById("nDBtarea");
+const ind = document.getElementById('index')
+
+async function misDatos(data) {
+  contenido.innerHTML = "";
+  await data.map((item, index) => {
+    // console.log(item._id)
+    contenido.innerHTML += `
+            <tr>
+              <th id="index" scope="row">${index + 1}</th>
+              
+              <td >${item.name}</td>
+              <td>${item.descripcion}</td>
+              <td>
+                ${item.statusTarea ? "pendiente" : "hecho"}
+              </td> 
+              <td onclick ="deleteTask('${
+                item._id
+              }')"><button  id="eliminarBtn" class="btn btn-danger">
+            <i class="fa-solid fa-trash-can"></i>
+          </button></td>
+
+          ${
+            item.statusTarea
+              ? `<td>
+            <button  id="eliminar" class="btn btn-warning">
+            <i class="fa-regular fa-pen-to-square"></i>
+             </button>
+          </td>`
+              : null
+          }
+          
+        `;
+  });
+}
+
+async function deleteTask(id) {
+  const response = await fetch(`http://localhost:3000/api/tareas/${id}`, {
+    method: "DELETE",
+  }).then(getDatos);
+  if (response) {
+    console.log(`efectivo el delete de ${id}`);
+  } else {
+    console.log("error en el delete");
+  }
+}
+
+document.getElementById("miapp").addEventListener("click", function (event) {
+  event.preventDefault();
+});
